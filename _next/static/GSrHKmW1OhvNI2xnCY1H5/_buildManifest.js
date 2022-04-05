dec7fe1aafac708eaf2b962bd271fbda7a9fe241
uiop49 <43157727+uiop49@users.noreply.github.com>
2022-04-05 00:22:00 +0000

    Deploying to gh-pages from @ uiop49/uiop49.github.io@ebbeedf8fa411ff8311e71b7c22fbb3d760abe03 🚀

---
+++ _next/static/GSrHKmW1OhvNI2xnCY1H5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5bc34d1fb2b67964.js"],"/_error":["static/chunks/pages/_error-e526a695839b5a36.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/agGHvWDPUZaXHAhsrFhn-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d9dcff8fd178bb8a.js"],"/_error":["static/chunks/pages/_error-e526a695839b5a36.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
